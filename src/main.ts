89bbc9b414f648c10b8e065d7180a498ea76ec78
Luis Alejandro Reyes MOrales <inglreyesm@gmail.com>
2021-10-24 17:44:55 +0200

    Adding cors

--- src/main.ts
+++ src/main.ts
@@ -3,9 +3,9 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-    const app = await NestFactory.create(AppModule,);
+    const app = await NestFactory.create(AppModule);
     app.enableCors({
-        origin: ['http://localhost:4200'],
+        origin: ['http://localhost:4200', 'https://handson-table.vercel.app'],
     });
     app.setGlobalPrefix('api');
     app.useGlobalPipes(new ValidationPipe());
