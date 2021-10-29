ff1cca70e0507beee432c4bb484fb6140e4fabcd
Luis Alejandro Reyes MOrales <inglreyesm@gmail.com>
2021-10-30 00:05:07 +0200

    Firebase functions deploy working - Update .firebaserc, firebase.json, and 3 more files...

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,21 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import { ExpressAdapter, NestExpressApplication } from '@nestjs/platform-express';
+import * as express from 'express';
+import * as functions from 'firebase-functions';
 import { AppModule } from './app.module';
-
-async function bootstrap() {
-    const app = await NestFactory.create(AppModule);
+const server: express.Express = express();
+export const createNestServer = async (expressInstance: express.Express) => {
+    const adapter = new ExpressAdapter(expressInstance);
+    const app = await NestFactory.create<NestExpressApplication>(AppModule, adapter, {});
     app.enableCors({
-        origin: ['http://localhost:4200', 'https://handson-table.vercel.app'],
+        origin: ['http://localhost:4200', 'https://handson-table.web.app'],
     });
-    app.setGlobalPrefix('api');
+    // app.setGlobalPrefix('api'); //this is not needed the export function name already contains 'api'
     app.useGlobalPipes(new ValidationPipe());
-    await app.listen(3000);
-}
-bootstrap();
+    return app.init();
+};
+createNestServer(server)
+    .then((v) => console.log('Nest Ready'))
+    .catch((err) => console.error('Nest broken', err));
+export const api: functions.HttpsFunction = functions.https.onRequest(server);
